309b62364390fecfad1ca8b30b22be0de84a358b
Michael <ainovatordev@gmail.com>
2024-03-01 00:54:27 +0300

    ready to len

--- base.py
+++ base.py
@@ -1,36 +1,36 @@
 import random
 import time
 
+#decor function to show time manage
 def decor_timer(funct):
     def timer():
         start_time = time.time()
         funct()
         end_time = time.time()
         print("Time load: ", end_time-start_time, "\n")
-    return timer
-
-
+    return funct
+#function to take only num
 def select_num():
     end_target = False
     while not end_target:
         try: 
             first_target = int(input("Input num: "))
-            print("Ok, you num is done")
+            print("\n")
             end_target = True
             
         except:
             print("Input only nums")        
     return first_target
 
+#function to generate list with random nums
 @decor_timer
 def generate_random_list():
     sep_list = []
-    random_len = random.randint(5,10)
+    random_len = random.randint(5, 10)
     for i in range(random_len):
-        sep_list.append(random.randint(1,9))
-    print("You list is generated")
+        sep_list.append(random.randint(1, 9))
+    print("Your list is generated \n")
     return sep_list
-
 #Function to check input y/n
 def step_check(input):
     try:
@@ -39,7 +39,7 @@ def step_check(input):
         input_lower = input
 
     if input_lower == "y" or input_lower == "n":
-        print("okey, lets do it")
+        print("Lets do it \n")
         return input_lower
     else:
         print('Input only "y" or "n"')
--- main.py
+++ main.py
@@ -1,64 +1,46 @@
 import sqlite3 as sq
 from base import select_num, generate_random_list, step_check
+#global input next step
+input_step = None
 
-con = sq.connect('db.db')
-cur = con.cursor()
 
 class Solution(object):
-    def __init__(self, nums=None, target=None):
-        self.nums = nums or []  # Инициализация nums, если None
+    def __init__(self, nums = [], target=None):
+        self.nums = nums
         self.target = target
-
-    def change_target(self):
-        end_this = False
-        while not end_this:
-            try:
-                new_target = int(input("You target is: "))
-                self.target = new_target
-                end_this = True
-                print("\n")
-            except ValueError:
-                print("Input only num")
-
+    #method to create new list with nums
     def new_nums(self):
         end_this = False
         while not end_this:
-            try:
-                len_adder = int(input("Input length of nums list: "))
-                for i in range(len_adder):
-                    print_assist = f"Input {i} num: "
-                    num_adder = int(input(print_assist))
-                    self.nums.append(num_adder)
-                end_this = True
-                print("\n")
-            except ValueError:
-                print("Input only nums")
-
+            print("Input list lenght: ")
+            len_adder = select_num()
+            for i in range(len_adder):
+                print(f"Input {i} num: ")
+                num_adder = select_num()
+                self.nums.append(num_adder)
+            end_this = True
+            print("\n")
+    #method to show all atribute info
     def display_all_info(self):
         print(f"You target is: {self.target}.")
         print(f"You num list is: {self.nums}")
 
-def first_step():
-    if input_step == "y":
-        list_set = generate_random_list()
-    elif input_step == "n":
-        new_object.new_nums()
+def find_sum(list_nums):
+    for num in range(len(list_nums)):
+        print(num)
 
-end_program = False
-while not end_program:
-    target_set = select_num()
-    new_object = Solution(target=target_set)
 
-    input_step = ""
-    while not step_check(input_step):
-        input_step = input("Do you want a random list? y/n | ")
+print("<Input target in next line>")
+target_set = select_num()
+new_object = Solution(target=target_set)
 
-    first_step()
-    new_object.display_all_info()
-    # Проверка наличия чисел в списке перед записью в базу данных
-    if new_object.nums:
-        for num in new_object.nums:
-            cur.execute('INSERT INTO list (num) VALUES (?)', (num,))
-        con.commit()  # Исправлен вызов метода commit
-    end_program = True
+while not step_check(input_step):
+    input_step = input("<Do you want random list? (y/n)>")
 
+if input_step == "y":
+    new_object.nums.extend(generate_random_list())
+else:
+    new_object.new_nums()
+
+find_sum(new_object.nums)
+new_object.display_all_info()
