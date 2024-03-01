2852e451524a57a62d78614c281c88fd0647f986
Michael <ainovatordev@gmail.com>
2024-03-01 20:13:00 +0300

    syntax error

---
+++ Scobe.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isValid(self,x):
+        
+        if x.count("()") 
+            return True 
+        else:
+            return False
+
+object = Solution()
+print(object.isValid("\'\'"))
