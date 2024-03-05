bfb970bccb4056d58050863d7482b1e9319963d6
Michael <ainovatordev@gmail.com>
2024-03-05 17:22:37 +0300

    New archeticture

---
+++ Problems/P1.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i, num in enumerate(nums):
+            # Вычисление второго слагаемого
+            partner_num = target - num
+            # Если число есть, добавляем в массив количество вариаций
+            if partner_num in nums[i+1:]:
+                # Используем enumerate для получения индексов
+                partner_index = nums.index(partner_num, i+1)
+                return i, partner_index
+            else:
+                pass
+
+object = Solution()
+answer = object.twoSum([3, 3], 6)
+print(answer)
+
