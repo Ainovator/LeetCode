9de0b72ed35378ec25d101d1781accba173c1fc5
Michael <ainovatordev@gmail.com>
2024-03-07 23:07:14 +0300

    binary

--- PlusOne.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def isValid(self,x):
-        if x.count("\'") == 2:
-            return True
-        else:
-            return False
-
-object = Solution()
-print(object.isValid("\'\'"))
